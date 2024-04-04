5861ee51274a330516802f84e09e885b42b195b6
csaba-nagy <dev.csaba.nagy@gmail.com>
2024-04-04 20:00:18 +0200

    refactor: extract the onSubmit function from register form

---
+++ utils/on-submit.ts
@@ -0,0 +1,12 @@
+import type { z } from 'zod'
+
+import type { UserWithPassword } from '~/types'
+
+import type { FormSubmitEvent } from '#ui/types'
+
+// TODO: add better type definition
+type FormSchema = z.output<typeof registerFormSchema> | z.output<typeof loginFormSchema>
+
+export function onSubmit({ data }: FormSubmitEvent<FormSchema>, callback: (data: Omit<UserWithPassword, 'id'>) => void): void {
+  callback(data)
+}
