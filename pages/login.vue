32cdd2dbc68064453ceef63c32b0cf9e9343042e
csaba-nagy <dev.csaba.nagy@gmail.com>
2024-04-09 11:42:57 +0200

    fix: use inline method to handle submit event on login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onSubmit } from '@/utils/on-submit'
+const { login } = useAuthStore()
 
 definePageMeta({
   middleware: ['02-authenticated'],
@@ -12,6 +12,6 @@ useHead({
 
 <template>
   <div class="grid place-items-center h-[calc(100vh-49px-16px)]">
-    <LoginForm @submit="onSubmit" />
+    <LoginForm @submit="({ data }) => login(data)" />
   </div>
 </template>
